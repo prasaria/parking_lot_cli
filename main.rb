2fd781a8ecea4b637daef8b013c2aaa4476c0be2
Prasetya Aria Wibawa <prasetya.ariawibawa@gmail.com>
2025-03-10 13:20:10 +0700

    initialize main file for entry point

---
+++ main.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+
+require_relative 'lib/models/parking_complex'
+require_relative 'lib/cli/menu'
+
+# Will be implemented later
+puts "OO Parking Lot CLI Application"
+puts "Setup not yet implemented"
