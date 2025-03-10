32b6eee8a2b9a66ddcf5e158ea318821b0ac5f21
Prasetya Aria Wibawa <prasetya.ariawibawa@gmail.com>
2025-03-10 23:36:21 +0700

    use composite index for parking ticket object id

--- lib/repositories/in_memory_repository.rb
+++ lib/repositories/in_memory_repository.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'monitor'
+require 'models/parking_ticket'
 
 # Repository that stores objects in memory
 # Provides basic CRUD operations for domain objects
@@ -26,7 +27,7 @@ class InMemoryRepository
     validate_object(object)
 
     @monitor.synchronize do
-      @objects[object.class][object.id] = object
+      @objects[object.class][object_id_for(object)] = object
     end
 
     object
@@ -65,7 +66,7 @@ class InMemoryRepository
     raise ArgumentError, 'Object cannot be nil' if object.nil?
 
     @monitor.synchronize do
-      @objects[object.class].delete(object.id)
+      @objects[object.class].delete(object_id_for(object))
     end
   end
 
@@ -73,6 +74,30 @@ class InMemoryRepository
   def clear
     @monitor.synchronize do
       @objects.clear
+    end
+  end
+
+  # Count the number of objects of a given type
+  # @param type [Class] The type of the objects to count
+  # @return [Integer] The number of objects of the given type
+  # @raise [ArgumentError] If type is nil
+  def count(type)
+    validate_type(type)
+
+    @monitor.synchronize do
+      @objects[type].size
+    end
+  end
+
+  # Check if an object exists in the repository
+  # @param object [Object] The object to check
+  # @return [Boolean] true if the object exists, false otherwise
+  # @raise [ArgumentError] If the object is nil
+  def exists?(object)
+    raise ArgumentError, 'Object cannot be nil' if object.nil?
+
+    @monitor.synchronize do
+      @objects[object.class].key?(object_id_for(object))
     end
   end
 
@@ -84,7 +109,7 @@ class InMemoryRepository
   def validate_object(object)
     if object.nil?
       raise ArgumentError, 'Object cannot be nil'
-    elsif !object.respond_to?(:id)
+    elsif !object.respond_to?(:id) && !object.is_a?(::ParkingTicket)
       raise ArgumentError, 'Object must have an id'
     end
   end
@@ -94,5 +119,18 @@ class InMemoryRepository
   # @raise [ArgumentError] If the type is nil
   def validate_type(type)
     raise ArgumentError, 'Type cannot be nil' if type.nil?
+  end
+
+  # Get the appropriate id for an object
+  # For ParkingTicket objects, use a composite key of vehicle.id, entry_time, and slot.id
+  # @param object [Object] The object to get the id for
+  # @return [Object] The id
+  def object_id_for(object)
+    if object.is_a?(::ParkingTicket)
+      # Create a composite key that uniquely identifies the ticket
+      "#{object.vehicle.id}_#{object.entry_time.to_i}_#{object.slot.id}"
+    else
+      object.id
+    end
   end
 end
