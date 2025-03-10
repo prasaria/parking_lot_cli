13bd2fdbef652e542dd139a41385beff3e20d6f7
Prasetya Aria Wibawa <prasetya.ariawibawa@gmail.com>
2025-03-10 14:07:37 +0700

    chore: rubocop styling

--- main.rb
+++ main.rb
@@ -1,8 +1,9 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
 require_relative 'lib/models/parking_complex'
 require_relative 'lib/cli/menu'
 
 # Will be implemented later
-puts "OO Parking Lot CLI Application"
-puts "Setup not yet implemented"
+puts 'OO Parking Lot CLI Application'
+puts 'Setup not yet implemented'
